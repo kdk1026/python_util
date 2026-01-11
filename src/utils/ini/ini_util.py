84bf6b0ff93513b6a4f63df364ac3d57d49c8d69
kdk1026 <daekwang1026@gmail.com>
2026-01-11 23:12:13 +0900

    업데이트

---
+++ src/utils/ini/ini_util.py
@@ -0,0 +1,237 @@
+import configparser
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class IniUtil:
+    is_null_or_empty = "{} is null or empty"
+
+    @classmethod
+    def get_ini(cls, file_path: str, section: str, key: str) -> str:
+        """ini 파일 읽기
+
+        Args:
+            file_path (str): _description_
+            section (str): _description_
+            key (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+            ValueError: _description_
+
+        Returns:
+            str: _description_
+        """
+
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not section or not section.strip():
+            raise ValueError(cls.is_null_or_empty.format("section"))
+
+        if not key or not key.strip():
+            raise ValueError(cls.is_null_or_empty.format("key"))
+
+        config = configparser.ConfigParser()
+
+        config.read(file_path, encoding="utf-8")
+
+        return config[section][key]
+
+    @classmethod
+    def get_ini_section(cls, file_path: str, section: str) -> list:
+        """ini 파일 읽기
+
+        Args:
+            file_path (str): _description_
+            section (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+
+        Returns:
+            list: _description_
+        """
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not section or not section.strip():
+            raise ValueError(cls.is_null_or_empty.format("section"))
+
+        config = configparser.ConfigParser()
+
+        config.read(file_path, encoding="utf-8")
+
+        return config.options(section)
+
+    @classmethod
+    def get_ini_key(cls, file_path: str, key: str) -> str:
+        """ini 파일 읽기
+
+        Args:
+            file_path (str): _description_
+            key (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+
+        Returns:
+            str: _description_
+        """
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not key or not key.strip():
+            raise ValueError(cls.is_null_or_empty.format("key"))
+
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                config_string = "[dummy]\n" + f.read()
+
+            config = configparser.ConfigParser()
+
+            config.read_string(config_string)
+
+            return config.get("dummy", key)
+        except FileNotFoundError:
+            logger.error(f"Error: File not found at {file_path}")
+
+    @classmethod
+    def add_ini(cls, file_path: str, section: str, key: str, value: str) -> None:
+        """ini 파일에 추가
+
+        Args:
+            file_path (str): _description_
+            section (str): _description_
+            key (str): _description_
+            value (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+            ValueError: _description_
+            ValueError: _description_
+        """
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not section or not section.strip():
+            raise ValueError(cls.is_null_or_empty.format("section"))
+
+        if not key or not key.strip():
+            raise ValueError(cls.is_null_or_empty.format("key"))
+
+        if not value or not value.strip():
+            raise ValueError(cls.is_null_or_empty.format("value"))
+
+        config = configparser.ConfigParser()
+
+        config.read(file_path, encoding="utf-8")
+
+        if not config.has_section(section):
+            config.add_section(section)
+
+        config.set(section, key, value)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            config.write(f)
+
+    @classmethod
+    def set_ini(cls, file_path: str, section: str, key: str, value: str) -> None:
+        """ini 파일에서 기존 섹션, 키의 값 변경
+
+        Args:
+            file_path (str): _description_
+            section (str): _description_
+            key (str): _description_
+            value (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+            ValueError: _description_
+            ValueError: _description_
+        """
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not section or not section.strip():
+            raise ValueError(cls.is_null_or_empty.format("section"))
+
+        if not key or not key.strip():
+            raise ValueError(cls.is_null_or_empty.format("key"))
+
+        if not value or not value.strip():
+            raise ValueError(cls.is_null_or_empty.format("value"))
+
+        config = configparser.ConfigParser()
+
+        config.read(file_path, encoding="utf-8")
+
+        config.set(section, key, value)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            config.write(f)
+
+    @classmethod
+    def clear_ini(cls, file_path: str, section: str) -> None:
+        """ini 파일에서 섹션 전체 삭제
+
+        Args:
+            file_path (str): _description_
+            section (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+        """
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not section or not section.strip():
+            raise ValueError(cls.is_null_or_empty.format("section"))
+
+        config = configparser.ConfigParser()
+
+        config.read(file_path, encoding="utf-8")
+
+        config.remove_section(section)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            config.write(f)
+
+    @classmethod
+    def clear_ini_key(cls, file_path: str, section: str, key: str) -> None:
+        """ini 파일에서 특정 키 삭제
+
+        Args:
+            file_path (str): _description_
+            section (str): _description_
+            key (str): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+            ValueError: _description_
+        """
+        if not file_path or not file_path.strip():
+            raise ValueError(cls.is_null_or_empty.format("file_path"))
+
+        if not section or not section.strip():
+            raise ValueError(cls.is_null_or_empty.format("section"))
+
+        if not key or not key.strip():
+            raise ValueError(cls.is_null_or_empty.format("key"))
+
+        config = configparser.ConfigParser()
+
+        config.read(file_path, encoding="utf-8")
+
+        config.remove_option(section, key)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            config.write(f)
