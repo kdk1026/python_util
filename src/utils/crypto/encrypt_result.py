dac69289da43e246703054e60fdf62afbd42e276
kdk1026 <daekwang1026@gmail.com>
2026-01-13 01:26:34 +0900

    업데이트

--- src/utils/crypto/encrypt_result.py
+++ src/utils/crypto/encrypt_result.py
@@ -1,5 +1,5 @@
 class EncryptResult:
-    def __init__(self, encrypted_text: bytes | str, iv: str, tag: bytes | None):
+    def __init__(self, encrypted_text: bytes | str, iv: str, tag: bytes | None = None):
         self.cipher_text = encrypted_text
         self.iv = iv
         self.tag = tag
