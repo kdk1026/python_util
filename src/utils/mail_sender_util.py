ed8dde1e88d30585d43c34e3c7427a3bfe123bfd
kdk1026 <daekwang1026@gmail.com>
2026-01-13 18:16:06 +0900

    업데이트

--- src/utils/mail_sender_util.py
+++ src/utils/mail_sender_util.py
@@ -18,7 +18,7 @@ class MailSenderUtil:
         section: str,
         subject: str,
         body: str,
-        to_email: str,
+        to_emails: list,
         attachment_path: str | None = None,
     ) -> bool:
         """메일 발송
@@ -33,7 +33,7 @@ class MailSenderUtil:
             section (str): _description_
             subject (str): _description_
             body (str): _description_
-            to_email (str): _description_
+            to_emails (list): _description_
             attachment_path (str | None, optional): _description_. Defaults to None.
 
         Raises:
@@ -55,37 +55,39 @@ class MailSenderUtil:
         if not body or not body.strip():
             raise ValueError(cls.__is_null_or_empty.format("body"))
 
-        if not to_email or not to_email.strip():
+        if not to_emails or len(to_emails) < 1:
             raise ValueError(cls.__is_null_or_empty.format("body"))
-
-        msg = EmailMessage()
-        msg["Subject"] = subject
-        msg["From"] = config_dict.get("username")
-        msg["To"] = to_email
-        msg.set_content(body)
-
-        if attachment_path:
-            file_path = Path(attachment_path)
-
-            if file_path.exists():
-                with open(attachment_path, "rb") as f:
-                    file_data = f.read()
-                    file_name = file_path.name
-
-                msg.add_attachment(
-                    file_data,
-                    maintype="application",
-                    subtype="octet-stream",
-                    filename=file_name,
-                )
 
         try:
             with smtplib.SMTP_SSL(
                 config_dict.get("host"), config_dict.get("port")
             ) as smtp:
                 smtp.login(config_dict.get("username"), config_dict.get("password"))
-                smtp.send_message(msg)
-                return True
+
+                for to_email in to_emails:
+                    msg = EmailMessage()
+                    msg["Subject"] = subject
+                    msg["From"] = config_dict.get("username")
+                    msg["To"] = to_email
+                    msg.set_content(body)
+
+                    if attachment_path:
+                        file_path = Path(attachment_path)
+
+                        if file_path.exists():
+                            with open(attachment_path, "rb") as f:
+                                file_data = f.read()
+                                file_name = file_path.name
+
+                            msg.add_attachment(
+                                file_data,
+                                maintype="application",
+                                subtype="octet-stream",
+                                filename=file_name,
+                            )
+
+                        smtp.send_message(msg)
+                        return True
         except Exception as e:
             logger.error(f"메일 발송 실패: {e}")
             return False
