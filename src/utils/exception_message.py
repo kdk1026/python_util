c7cac5163fd55d4d36705fb08bf55fbfa76c9723
kdk1026 <daekwang1026@gmail.com>
2026-01-13 16:10:24 +0900

    업데이트

---
+++ src/utils/exception_message.py
@@ -0,0 +1,16 @@
+class ExceptionMessage:
+    @staticmethod
+    def is_null(param_name: str) -> str:
+        return "{} is null".format(param_name)
+
+    @staticmethod
+    def in_valid(param_name: str) -> str:
+        return "{} is inValid".format(param_name)
+
+    @staticmethod
+    def is_null_or_empty(param_name: str) -> str:
+        return "{} is null or empty".format(param_name)
+
+    @staticmethod
+    def is_negative(param_name: str) -> str:
+        return "{} is negative".format(param_name)
